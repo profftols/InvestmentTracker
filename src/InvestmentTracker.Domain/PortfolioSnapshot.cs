2b7446c78d53171093f3c480eb3268e87e07a239
IK-PC <i.korikov.91@gmail.com>
2026-03-21 21:46:37 +0500

    Add PortfolioSnapshot domain entity
    Added `PortfolioSnapshot` class to the Domain project with `Id`, `AccountId`, `TotalValue`, and `Date` properties.

---
+++ src/InvestmentTracker.Domain/PortfolioSnapshot.cs
@@ -0,0 +1,9 @@
+﻿namespace InvestmentTracker.Domain;
+
+public class PortfolioSnapshot
+{
+    public Guid Id { get; private set; }
+    public Guid AccountId { get; private set; }
+    public decimal TotalValue { get; private set; }
+    public DateTime Date { get; private set; }
+}
